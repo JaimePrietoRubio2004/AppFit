c27e918e355ddedba0e60b10e2dd27b68f1df7ee
JaimePrietoRubio2004 <jaimeprietorubio@gmail.com>
2026-09-15 19:58:52 +0200

    Proyecto Ionic 9 + Angular 22 standalone generado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'APP',
+  appId: 'com.jaime.appfit',
+  appName: 'AppFit',
   webDir: 'www'
 };
 
